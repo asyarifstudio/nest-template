e8a5f8f8af72e43271f67899ceecbe7f2ea5931e
sahmad <syarif.ahmad@thalesgroup.com>
2022-07-29 01:17:54 +0800

    create simple guard

---
+++ src/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import {Request } from 'express'
+import { AuthService } from 'src/services/auth/auth.service';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+
+  constructor(private authService:AuthService){}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request:Request = context.switchToHttp().getRequest();
+    return this.verify(request);
+  }
+
+
+  async verify(request:Request):Promise<boolean>{
+    const token:string = request.headers.authorization;
+    let user = await this.authService.verify(token);
+    request.user = user;
+    return user!=undefined;
+  }
+}
