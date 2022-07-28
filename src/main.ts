6a93cfb9277293886aed0e72f6af287199608c92
sahmad <syarif.ahmad@thalesgroup.com>
2022-07-29 01:12:16 +0800

    integrate the firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import { environment } from 'env.prod';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+
+const server:express.Express = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  admin.initializeApp({
+    credential:admin.credential.cert(environment.firebase.serviceAccount),
+    databaseURL:environment.firebase.databaseUrl
+  })
+  const adapter = new ExpressAdapter(server);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,adapter,{}
+  )
+
+  app.enableCors();
+  if(!environment.production){
+    return app.listen(3000);
+  }
+  else{
+    return app.init();
+  }
 }
-bootstrap();
+
+
+
+bootstrap()
+  .then(v => console.log('Nest Ready'))
+  .catch(err => console.error('Nest broken', err));
+export const api: functions.HttpsFunction = functions.https.onRequest(server);
