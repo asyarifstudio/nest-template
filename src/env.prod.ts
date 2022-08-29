29e49e5b52b6da517cb810b1bfc09a19d47f7e84
sahmad <syarif.ahmad@thalesgroup.com>
2022-08-30 07:23:40 +0800

    add comment how to get the firebase secret

--- src/env.prod.ts
+++ src/env.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production:true,
     firebase:{
+        //generate the services account in firebase console 
         serviceAccount:{},
         databaseUrl:''
     }
